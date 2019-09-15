a2d361e31782cb1f28648363c5291a2b5722eb9e
patrick-holt <patrick1995holt@gmail.com>
2019-09-15 09:38:54 +0200

    Add function to process commands

--- ingame-mover.js
+++ ingame-mover.js
@@ -1,23 +1,47 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 // For use on my own system
-//const auth = require('./auth.json');
+const auth = require('./auth.json');
 
 //Channel IDs
 const generalVoice = '602869084085944334';
 const ingameVoice = '603324302703591457';
+const roleAdminID = '622690871153786901';
 
+// Event, when bot first runs
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  // Print all names, id's, and roles
+  client.guilds.forEach((guild) => {
+    guild.members.forEach((member) => {
+      console.log(`${member.displayName} ${member.id} ${member.highestRole.id}`);
+      if (member.highestRole.id === roleAdminID) {
+        console.log(`${member.displayName} is an Admin`);
+      }
+    })
+  })
 });
 
+// Event, whenever a message is received 
 client.on('message', msg => {
+  // Bot should never respond to itself
+  if (msg.author === client.user) {
+    return;
+  }
+
+  // We handle commands differently
+  if (msg.content.startsWith('!')) {
+    ProcessCommand(msg);
+  }
+
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
 
   // If Musk tries to be sexy with the bot he will succeed
   if (msg.content === 'sup bot?' && msg.author.username === 'Musk') {
+    // I am aware that people can cheat by changing their names, maybe use client ID instead?
     msg.reply('Whattup your sexy husk of meat?');
   // No one else will
   } else if (msg.content === 'sup bot?') {
@@ -47,6 +71,22 @@ client.on('presenceUpdate', (oldMember, newMember) => {
   }
 })
 
+//
+// Functions
+//
+function ProcessCommand(receivedMessage) {
+  let fullCommand = msg.content.substr(1); // Remove exclamationmark
+  let splitCommand = fullCommand.split(" "); // Split up message
+  let primaryCommand = splitCommand[0]; // First word is the command
+  let arguments = splitCommand.slice(1); // All other words are arguments
+
+  console.log("Command received: " + primaryCommand)
+  console.log("Arguments: " + arguments) // There may not be any arguments
+}
+//
+//
+//
+
 // Use the first one if on my own system, second is for Heroku
-//client.login(auth.token);
-client.login(process.env.CLIENT_TOKEN);
+client.login(auth.token);
+//client.login(process.env.CLIENT_TOKEN);
