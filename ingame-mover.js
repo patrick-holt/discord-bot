502f51c085a1250cba7a52b8534f7fd0a70f6cc4
patrick-holt <patrick1995holt@gmail.com>
2019-09-22 16:41:37 +0200

    Add answer for 'sup bot?'

--- ingame-mover.js
+++ ingame-mover.js
@@ -47,6 +47,8 @@ client.on('message', msg => {
   if (msg.content === 'sup bot?' && msg.author.username === 'Musk') {
     // I am aware that people can cheat by changing their names, maybe use client ID instead?
     msg.reply('Whattup your sexy husk of meat?');
+  } else if (msg.content === 'sup bot?') {
+    msg.reply("You're not my summer prince...")
   }
 
   // If Mikkel tries something
